407a0c48003be017b6702dd8df048406569b2617
jakub.dupak <dupak@praguebest.cz>
2016-10-07 17:14:10 +0200

    úkol seminář - nový

---
+++ prvocislo.php
@@ -0,0 +1,22 @@
+<form method="get">
+<input name="n">
+<input type="submit">
+</form>
+<?php 
+$n = $_GET["n"];
+function prvociclo($n){
+	$sq_n = sqrt($n);
+	for ($i=2; $i <= $sq_n; $i++) { 
+		if ($n % $i == 0) {
+			return 1;
+		}
+	}
+}
+$mark = prvociclo($n);
+if ($mark == 1) {
+	echo "Neni prvocislo";
+} else {
+	echo "Je prvocislo";
+}
+
+?>
