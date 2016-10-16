89f46cd3bd4ca890327f6a13d3c1a2799a68720f
HollMax <hollmannm1@gmail.com>
2016-10-16 18:32:12 +0200

    omezeni casem, misto poctem iteraci

--- pi.php
+++ pi.php
@@ -1,5 +1,7 @@
 <?php 
-	for ($i=0; $i < 1000000; $i++) { 
+	$i=0
+	$t=2	//doba po kterou pobezi for
+	for ($e=time(); time() <= ($e + $t); $i++) { 
 		$pi += (4*pow(-1, $i)/(2*$i+1));
 	}
 	echo($pi);
