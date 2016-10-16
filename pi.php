beb1a6c1f4cfb003f8816075e176926b1709f993
HollMax <hollmannm1@gmail.com>
2016-10-16 18:53:51 +0200

    optimalizace (30x vyssi vykon)

--- pi.php
+++ pi.php
@@ -2,7 +2,7 @@
 	$i=0
 	$t=2	//doba po kterou pobezi for
 	for ($e=time(); time() <= ($e + $t); $i++) { 
-		$pi += (4*pow(-1, $i)/(2*$i+1));
+		$pi += ((4 - 8 * ($i % 2))/(2*$i+1));
 	}
 	echo($pi);
 ?>
