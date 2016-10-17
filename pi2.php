3aa30850b509e1832d4dfc3eee75074b076e69ef
jdupak <sk.dupak@gmail.com>
2016-10-17 20:20:15 +0200

    edit pi2.php

--- pi2.php
+++ pi2.php
@@ -9,11 +9,6 @@
 	for ($k=0; $k < 10; $k++) { 
 		$z += (fact(6*$k)*(13591409+545140134*$k)/(fact(3*$k)*pow(fact($k), 3)*pow(-640320,3*$k)));
 	}
-	echo $z;
-	echo "<br>";
 	$pi = (426880*sqrt(10005))/$z;
 	echo($pi);
-	$k=0;
-	echo "<br>".(fact(3*$k)*pow(fact($k), 3)*pow(-640320,3*$k));
-	echo "<br>".fact(6*$k)*(13591409+545140134*$k);
 	?>
