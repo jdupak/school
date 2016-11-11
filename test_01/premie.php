fde5cdd6024dc6e5942717979185712139a3381d
jdupak <sk.dupak@gmail.com>
2016-11-11 14:57:40 +0100

    Premie extended funkční

--- test_01/premie.php
+++ test_01/premie.php
@@ -2,16 +2,16 @@
 	function toDec($input, $base)
 	{
 		$array = str_split($input);
-		array_reverse($array);
-   foreach ($array as $key =>  $value) {
-   	  if (is_integer($value) == false) {
-   	  	$value = ord(strtoupper($value)) - ord('A') + 11;
-   	  }
-   }           
+		$array = array_reverse($array);
+		foreach ($array as $key =>  $value) {
+			if (is_numeric($value) == false) {
+		   		$array["$key"] = ord(strtoupper($value)) - ord('A') + 10;
+		   	 }
+		}         
 		foreach ($array as $key => $value) {
 			$result += $value*pow($base, $key);
 		}
 		return $result;
 	}
-	echo(toDec(111111111,2));
+	echo(toDec(z,1));
 ?>
