4466a589b7bbee57b230364568b210abe8caba3f
jdupak <jdupak@users.noreply.github.com>
2016-11-11 12:51:47 +0100

    Premie ext
    Pro base<10

--- test_01/premie.php
+++ test_01/premie.php
@@ -3,6 +3,11 @@
 	{
 		$array = str_split($input);
 		array_reverse($array);
+   foreach ($array as $key =>  $value) {
+   	  if (is_integer($value) == false) {
+   	  	$value = ord(strtoupper($value)) - ord('A') + 11;
+   	  }
+   }           
 		foreach ($array as $key => $value) {
 			$result += $value*pow($base, $key);
 		}
