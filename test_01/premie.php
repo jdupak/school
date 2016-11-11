218fa3a82eb4e902c93ff7f9ee08253b64551301
jdupak <jdupak@users.noreply.github.com>
2016-11-11 09:20:30 +0100

    Test prémie

--- test_01/premie.php
+++ test_01/premie.php
@@ -1,8 +1,12 @@
 <?php
 	function toDec($input, $base)
 	{
-		$array = explode("",$input);
-		echo $array;
+		$array = str_split($input);
+		array_reverse($array);
+		foreach ($array as $key => $value) {
+			$result += $value*pow($base, $key);
+		}
+		return $result;
 	}
-	toDec(12549,1)
+	echo(toDec(111111111,2));
 ?>
